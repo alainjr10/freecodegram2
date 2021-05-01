fec4dc05a1c6eb1f1c9ae664f191f46313cffd44
alainjr10 <njokomalain@gmail.com>
2021-05-01 06:16:16 +0100

    Added a add new post button to profile page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,9 @@
             <img src="https://scontent-amt2-1.cdninstagram.com/v/t51.2885-19/s320x320/97566921_2973768799380412_5562195854791540736_n.jpg?tp=1&_nc_ht=scontent-amt2-1.cdninstagram.com&_nc_ohc=3wbAvxggGKoAX8jySKC&edm=ABfd0MgAAAAA&ccb=7-4&oh=a604a2955e371f44bb7d605276d28db8&oe=60A82CDF&_nc_sid=7bff83" alt="" class="rounded-circle" style="height: 150px;">
         </div>
         <div class="col-9 pt-5">
-            <div>
+            <div class="d-flex justify-content-between align-items-baseline">
                 <h1> {{ $user->username }}</h1> 
+                <a href="#">Add New Post</a>
             </div>
             <div class="d-flex">
                 <div class=pr-3><strong> 376 </strong> posts </div>
