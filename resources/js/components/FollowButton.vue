0dc0e7a28839a5c6ff7e5423138fd184cc17887c
alainjr10 <njokomalain@gmail.com>
2021-05-04 06:20:12 +0100

    handled following of users

---
+++ resources/js/components/FollowButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <button class="btn btn-primary ml-4" @click="followUser">Follow</button>
+    </div>
+</template>
+
+<script>
+    export default {
+
+        props: ['userId'],   
+
+        mounted() {
+            console.log('Component mounted.')
+        },
+
+        methods: {
+            followUser(){
+                axios.post('/follow/' + this.userId) //axios makes us make api calls. here, we are calling to the follow route
+                    .then(response =>{  //if succesfull, then ....
+                        console.log(response.data);
+                    });
+            }
+        }
+    }
+</script>
