93adaea62f12b1d03edd5fadb5b46c15267d0afe
alainjr10 <njokomalain@gmail.com>
2021-05-06 01:59:09 +0100

    finished setting up emails with mailtrap.io

---
+++ resources/views/emails/welcome-mail.blade.php
@@ -0,0 +1,9 @@
+@component('mail::message')
+# welcome to freeCodeGram
+
+We are glad to welcome you to this awesome community of developers.
+
+
+Cheers,<br>
+{{ config('app.name') }}
+@endcomponent
